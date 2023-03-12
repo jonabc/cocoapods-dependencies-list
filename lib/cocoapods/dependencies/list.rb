59a11b54b09ffaf3f24f6af545e179cc06afb711
Jon Ruskin <jonabc@github.com>
2023-03-12 13:54:11 -0700

    bundle gem

---
+++ lib/cocoapods/dependencies/list.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require_relative "list/version"
+
+module Cocoapods
+  module Dependencies
+    module List
+      class Error < StandardError; end
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/cocoapods/dependencies/list/version.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+module Cocoapods
+  module Dependencies
+    module List
+      VERSION = "0.1.0"
+    end
+  end
+end
