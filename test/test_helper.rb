b3639e6c327f2e37ff1ad6c403e106f7b5326d5e
Jon Ruskin <jonabc@github.com>
2023-03-12 22:51:39 -0700

    add test fixtures

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,7 +1,9 @@
 # frozen_string_literal: true
 
 $LOAD_PATH.unshift File.expand_path("../lib", __dir__)
+
+FIXTURES_DIRECTORY = File.expand_path("fixtures", __dir__)
+
 require "debug"
 require "cocoapods_dependencies_list"
-
 require "minitest/autorun"
