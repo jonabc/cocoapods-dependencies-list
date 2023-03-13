b9e87a3ad97ba9cfe910c7279576380601fa19ea
Jon Ruskin <jonabc@github.com>
2023-03-12 20:56:41 -0700

    fix require path

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
 $LOAD_PATH.unshift File.expand_path("../lib", __dir__)
-require "cocoapods/dependencies/list"
+require "cocoapods_dependencies_list"
 
 require "minitest/autorun"
